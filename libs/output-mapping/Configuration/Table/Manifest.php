339d9fd52fe50e875c6f43bb5b1b7c7e49202ae5
Odin <odinuv@gmail.com>
2017-06-20 19:39:05 +0200

    fix: remove default value for unused escaped_by parameter, the default value triggers erroneous warning

--- libs/output-mapping/Configuration/Table/Manifest.php
+++ libs/output-mapping/Configuration/Table/Manifest.php
@@ -43,7 +43,7 @@ class Manifest extends Configuration
                 ->end()
                 ->scalarNode("delimiter")->defaultValue(",")->end()
                 ->scalarNode("enclosure")->defaultValue("\"")->end()
-                ->scalarNode("escaped_by")->defaultValue("")->end() //TODO: escaped_by is deprecated and should not be used any more
+                ->scalarNode("escaped_by")->end() //TODO: escaped_by is deprecated and should not be used any more
                 ->arrayNode("metadata")
                     ->prototype('array')
                         ->children()

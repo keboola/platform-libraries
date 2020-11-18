b5303ed9b92b609b21cd5af2de44eed9089e99a4
pivnicek <marc@keboola.com>
2020-11-18 19:39:48 +0100

    fix tests for clientWrapper usage

--- libs/output-mapping/tests/Writer/AbsWriterWorkspaceTest.php
+++ libs/output-mapping/tests/Writer/AbsWriterWorkspaceTest.php
@@ -7,13 +7,61 @@ use Keboola\InputMapping\Reader\NullWorkspaceProvider;
 use Keboola\InputMapping\Reader\WorkspaceProviderInterface;
 use Keboola\OutputMapping\Exception\InvalidOutputException;
 use Keboola\OutputMapping\Writer\TableWriter;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Exception;
 use Keboola\StorageApi\Metadata;
 use Keboola\StorageApi\Workspaces;
+use Keboola\StorageApiBranch\ClientWrapper;
 use Keboola\Temp\Temp;
 use Psr\Log\NullLogger;
 
 class AbsWriterWorkspaceTest extends BaseWriterWorkspaceTest
 {
+    private $runSynapseTests = false;
+
+    public function setUp()
+    {
+        $this->runSynapseTests = getenv('RUN_SYNAPSE_TESTS');
+        if (!$this->runSynapseTests) {
+            return;
+        }
+        if (getenv('SYNAPSE_STORAGE_API_TOKEN') === false) {
+            throw new Exception('SYNAPSE_STORAGE_API_TOKEN must be set for synapse tests');
+        }
+        if (getenv('SYNAPSE_STORAGE_API_URL') === false) {
+            throw new Exception('SYNAPSE_STORAGE_API_URL must be set for synapse tests');
+        }
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        if (!$this->runSynapseTests) {
+            return;
+        }
+        parent::tearDown();
+    }
+
+    protected function initClient()
+    {
+        $token = (string) getenv('SYNAPSE_STORAGE_API_TOKEN');
+        $url = (string) getenv('SYNAPSE_STORAGE_API_URL');
+        $this->clientWrapper = new ClientWrapper(
+            new Client(["token" => $token, "url" => $url]),
+            null,
+            null
+        );
+        $tokenInfo = $this->clientWrapper->getBasicClient()->verifyToken();
+        print(sprintf(
+            'Authorized as "%s (%s)" to project "%s (%s)" at "%s" stack.',
+            $tokenInfo['description'],
+            $tokenInfo['id'],
+            $tokenInfo['owner']['name'],
+            $tokenInfo['owner']['id'],
+            $this->clientWrapper->getBasicClient()->getApiUrl()
+        ));
+    }
+
     public function testAbsTableOutputMapping()
     {
         $root = $this->tmp->getTmpFolder();
@@ -43,7 +91,7 @@ class AbsWriterWorkspaceTest extends BaseWriterWorkspaceTest
                 ['columns' => ['Id2', 'Name2']]
             )
         );
-        $writer = new TableWriter($this->client, new NullLogger(), $this->getWorkspaceProvider());
+        $writer = new TableWriter($this->clientWrapper, new NullLogger(), $this->getWorkspaceProvider());
 
         $tableQueue = $writer->uploadTables(
             $root,
@@ -54,7 +102,7 @@ class AbsWriterWorkspaceTest extends BaseWriterWorkspaceTest
         $jobIds = $tableQueue->waitForAll();
         $this->assertCount(2, $jobIds);
 
-        $tables = $this->client->listTables('out.c-output-mapping-test');
+        $tables = $this->clientWrapper->getBasicClient()->listTables('out.c-output-mapping-test');
         $this->assertCount(2, $tables);
         $tableIds = [$tables[0]['id'], $tables[1]['id']];
         sort($tableIds);
@@ -62,12 +110,12 @@ class AbsWriterWorkspaceTest extends BaseWriterWorkspaceTest
         $this->assertCount(2, $jobIds);
         $this->assertNotEmpty($jobIds[0]);
         $this->assertNotEmpty($jobIds[1]);
-        $job = $this->client->getJob($jobIds[0]);
+        $job = $this->clientWrapper->getBasicClient()->getJob($jobIds[0]);
         $this->assertEquals('out.c-output-mapping-test.table1a', $job['tableId']);
         $this->assertEquals(true, $job['operationParams']['params']['incremental']);
         $this->assertEquals(['Id'], $job['operationParams']['params']['columns']);
-        $data = $this->client->getTableDataPreview('out.c-output-mapping-test.table1a');
-        $job = $this->client->getJob($jobIds[1]);
+        $data = $this->clientWrapper->getBasicClient()->getTableDataPreview('out.c-output-mapping-test.table1a');
+        $job = $this->clientWrapper->getBasicClient()->getJob($jobIds[1]);
         $this->assertEquals('out.c-output-mapping-test.table2a', $job['tableId']);
         $this->assertEquals(false, $job['operationParams']['params']['incremental']);
         $this->assertEquals(['Id2', 'Name2'], $job['operationParams']['params']['columns']);
@@ -79,7 +127,7 @@ class AbsWriterWorkspaceTest extends BaseWriterWorkspaceTest
             'strtolower',
             $rows
         );
-        // Both id and name columns are present because of https://keboola.atlassian.net/browse/KBC-865
-        $this->assertEquals(['"id","name"', '"aabb","ccdd"', '"test","test"'], $rows);
+        // 1a has only the id column
+        $this->assertEquals(['"id"', '"test"'], $rows);
     }
 }

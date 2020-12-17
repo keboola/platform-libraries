0bb9f2acbf19a19b652367b390fff203996a94c0
pivnicek <marc@keboola.com>
2020-12-17 19:30:46 +0100

    Revert "use new option name"
    This reverts commit d8469a3d9327622c4e718ab0664c47d66b590da7.

--- libs/output-mapping/src/Writer/TableWriter.php
+++ libs/output-mapping/src/Writer/TableWriter.php
@@ -564,7 +564,7 @@ class TableWriter extends AbstractWriter
             $dataStorage = $this->strategy->getDataStorage();
             $options = [
                 'dataWorkspaceId' => $dataStorage->getWorkspaceId(),
-                'dataObject' => $sourcePath,
+                'dataTableName' => $sourcePath,
                 'incremental' => $options['incremental'],
                 'columns' => $options['columns'],
             ];

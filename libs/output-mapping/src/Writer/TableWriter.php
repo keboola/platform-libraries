d2654dea4ed0f700068b4f088b0347a68a952ba2
odin <odinuv@gmail.com>
2021-01-05 21:20:06 +0100

    fix: condition

--- libs/output-mapping/src/Writer/TableWriter.php
+++ libs/output-mapping/src/Writer/TableWriter.php
@@ -114,7 +114,7 @@ class TableWriter extends AbstractWriter
 
             if ($manifest !== false) {
                 $configFromManifest = $this->readTableManifest($manifest->getPathname());
-                if (empty($configFromManifest['destination']) || isset($configuration['bucket'])) {
+                if (empty($configFromManifest['destination']) && isset($configuration['bucket'])) {
                     $configFromManifest['destination'] = $this->createDestinationConfigParam(
                         $prefix,
                         $manifest->getBasename('.manifest')

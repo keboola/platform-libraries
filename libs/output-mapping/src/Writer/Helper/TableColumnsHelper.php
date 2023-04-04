24dbbab11a199f9f55f5df0dc3a7a5a58abdf85d
Erik Zigo <erik.zigo@keboola.com>
2023-04-05 01:21:52 +0200

    output-mapping: ColumnsHelper + TypedColumnsHelper merged into TableColumnsHelper

--- libs/output-mapping/src/Writer/Helper/TableColumnsHelper.php
+++ libs/output-mapping/src/Writer/Helper/TableColumnsHelper.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Keboola\OutputMapping\Writer\Helper;
 
+use Keboola\OutputMapping\Writer\Table\TableDefinition\TableDefinitionColumnFactory;
 use Keboola\StorageApi\Client;
+use Keboola\Utils\Sanitizer\ColumnNameSanitizer;
 
 class TableColumnsHelper
 {
@@ -14,10 +16,80 @@ class TableColumnsHelper
         array $newTableConfiguration,
         string $backendType,
     ): void {
+        $missingColumnsData = [];
+        $missingColumns = self::getMissingColumnsFromColumnMetadata($currentTableInfo, $newTableConfiguration);
+
         if ($currentTableInfo['isTyped'] === true) {
-            TypedColumnsHelper::addMissingColumns($client, $currentTableInfo, $newTableConfiguration, $backendType);
+            foreach ($newTableConfiguration['column_metadata'] as $columnName => $columnMetadata) {
+                $columnName = ColumnNameSanitizer::sanitize($columnName);
+
+                if (!in_array($columnName, $missingColumns)) {
+                    continue;
+                }
+
+                $tableMetadata = $newTableConfiguration['metadata'] ?? [];
+                $column = (new TableDefinitionColumnFactory($tableMetadata, $backendType))
+                    ->createTableDefinitionColumn($columnName, $columnMetadata);
+
+                $columnData = $column->toArray();
+                $missingColumnsData[] = [
+                    $column->getName(),
+                    $columnData['definition'] ?? null,
+                    $columnData['basetype'] ?? null,
+                ];
+            }
         } else {
-            ColumnsHelper::addMissingColumns($client, $currentTableInfo, $newTableConfiguration);
+            $missingColumns = !$missingColumns
+                ? self::getMissingColumnsFromColumns($currentTableInfo, $newTableConfiguration)
+                : $missingColumns;
+
+            foreach ($missingColumns as $columnName) {
+                $missingColumnsData[] = [
+                    $columnName,
+                    null,
+                    null,
+                ];
+            }
         }
+
+        foreach ($missingColumnsData as $missingColumnData) {
+            [$columnName, $columnDefinition, $columnBasetype] = $missingColumnData;
+            $client->addTableColumn(
+                $currentTableInfo['id'],
+                $columnName,
+                $columnDefinition,
+                $columnBasetype
+            );
+        }
+    }
+
+    private static function getMissingColumnsFromColumnMetadata(
+        array $currentTableInfo,
+        array $newTableConfiguration
+    ): array {
+        $tableColumns = $currentTableInfo['columns'];
+        $configColumns = [];
+
+        if (!empty($newTableConfiguration['column_metadata'])) {
+            $configColumns = array_map(function ($columnName): string {
+                return ColumnNameSanitizer::sanitize($columnName);
+            }, array_keys($newTableConfiguration['column_metadata']));
+        }
+
+        return array_diff($configColumns, $tableColumns);
+    }
+
+    private static function getMissingColumnsFromColumns(array $currentTableInfo, array $newTableConfiguration): array
+    {
+        $tableColumns = $currentTableInfo['columns'];
+        $configColumns = [];
+
+        if (!empty($newTableConfiguration['columns'])) {
+            $configColumns = array_map(function ($columnName): string {
+                return ColumnNameSanitizer::sanitize($columnName);
+            }, $newTableConfiguration['columns']);
+        }
+
+        return array_diff($configColumns, $tableColumns);
     }
 }

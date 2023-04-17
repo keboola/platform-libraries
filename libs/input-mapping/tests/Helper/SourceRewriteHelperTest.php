c40c12895969644e022f7f991bd59543eb6e3803
odin <odinuv@gmail.com>
2023-04-17 14:20:20 +0200

    use ascync drop for buckets

--- libs/input-mapping/tests/Helper/SourceRewriteHelperTest.php
+++ libs/input-mapping/tests/Helper/SourceRewriteHelperTest.php
@@ -64,12 +64,12 @@ class SourceRewriteHelperTest extends TestCase
             Client::STAGE_OUT
         );
         if ($outDevBucketId) {
-            $clientWrapper->getBasicClient()->dropBucket((string) $outDevBucketId, ['force' => true]);
+            $clientWrapper->getBasicClient()->dropBucket((string) $outDevBucketId, ['force' => true, 'async' => true]);
         }
 
         foreach ($clientWrapper->getBasicClient()->listBuckets() as $bucket) {
             if (preg_match('/^(c-)?[0-9]+-output-mapping-test$/ui', $bucket['name'])) {
-                $clientWrapper->getBasicClient()->dropBucket($bucket['id'], ['force' => true]);
+                $clientWrapper->getBasicClient()->dropBucket($bucket['id'], ['force' => true, 'async' => true]);
             }
         }
 

97b7279b16813faf81194aa647d4d1e992b46840
Erik Zigo <erik.zigo@keboola.com>
2023-04-19 10:50:17 +0200

    k8s-client: ConfigMapsApiClientFunctionalTest

---
+++ libs/k8s-client/tests/ApiClient/ConfigMapsApiClientFunctionalTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\K8sClient\Tests\ApiClient;
+
+use Keboola\K8sClient\ApiClient\ConfigMapsApiClient;
+use Kubernetes\API\ConfigMap as ConfigMapsApi;
+use Kubernetes\Model\Io\K8s\Api\Core\V1\ConfigMap;
+use PHPUnit\Framework\TestCase;
+
+class ConfigMapsApiClientFunctionalTest extends TestCase
+{
+    /**
+     * @template-use BaseNamespaceApiClientTestCase<ConfigMapsApi, ConfigMapsApiClient>
+     */
+    use BaseNamespaceApiClientTestCase;
+
+    public function setUp(): void
+    {
+        //@TODO resolve problems with root crt that is also saved in configmap in same namespace
+        parent::setUp();
+        $this->setUpBaseNamespaceApiClientTest(
+            ConfigMapsApi::class,
+            ConfigMapsApiClient::class,
+        );
+    }
+
+    protected function createResource(array $metadata): ConfigMap
+    {
+        return new ConfigMap([
+            'metadata' => $metadata,
+            'data' => [
+                'test_key' => 'test_value',
+            ],
+        ]);
+    }
+}

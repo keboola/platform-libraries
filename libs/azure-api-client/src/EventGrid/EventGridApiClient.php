66c9d3118a78b491b431f456e404229eb628124d
zajca <zajca@zajca.cz>
2023-04-20 09:29:53 +0200

    add event grid

---
+++ libs/azure-api-client/src/EventGrid/EventGridApiClient.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\AzureApiClient\EventGrid;
+
+use GuzzleHttp\Psr7\Request;
+use Keboola\AzureApiClient\ApiClient;
+use Keboola\AzureApiClient\ApiClientConfiguration;
+use Keboola\AzureApiClient\Authentication\Authenticator\CustomHeaderAuth;
+use Keboola\AzureApiClient\EventGrid\Model\EventGridEvent;
+use Webmozart\Assert\Assert;
+
+class EventGridApiClient
+{
+    private const API_VERSION = '2018-01-01';
+
+    private ApiClient $apiClient;
+
+    public function __construct(
+        string $topicHostname,
+        ?string $token = null,
+        ?ApiClientConfiguration $configuration = null,
+    ) {
+        if ($configuration === null) {
+            Assert::stringNotEmpty($token, 'Token must be set when no configuration is provided.');
+            $configuration = new ApiClientConfiguration(
+                authenticator: new CustomHeaderAuth(
+                    header: 'aeg-sas-key',
+                    value: $token,
+                ),
+            );
+        }
+        $this->apiClient = new ApiClient(
+            sprintf('https://%s', $topicHostname),
+            $configuration,
+        );
+        $this->apiClient->authenticate(Resources::AZURE_EVENT_GRID);
+    }
+
+    /**
+     * @param EventGridEvent[] $events
+     */
+    public function publishEvents(array $events): void
+    {
+        $this->apiClient->sendRequest(
+            new Request(
+                'POST',
+                sprintf('/api/events?api-version=%s', self::API_VERSION),
+                [],
+                (string) json_encode(array_map(
+                    static fn($event) => $event->toArray(),
+                    $events
+                ), JSON_THROW_ON_ERROR),
+            ),
+        );
+    }
+}

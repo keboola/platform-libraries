912ec43eb7cef03b425bb3a1136cecad23d72e8a
zajca <zajca@zajca.cz>
2023-04-20 12:17:04 +0200

    remove setting ApiClientConfiguration in EventGridApiClient

--- libs/azure-api-client/src/EventGrid/EventGridApiClient.php
+++ libs/azure-api-client/src/EventGrid/EventGridApiClient.php
@@ -7,10 +7,8 @@ namespace Keboola\AzureApiClient\EventGrid;
 use GuzzleHttp\Psr7\Request;
 use Keboola\AzureApiClient\ApiClient;
 use Keboola\AzureApiClient\ApiClientConfiguration;
-use Keboola\AzureApiClient\Authentication\Authenticator\CustomHeaderAuth;
 use Keboola\AzureApiClient\EventGrid\Model\EventGridEvent;
 use Keboola\AzureApiClient\Json;
-use Webmozart\Assert\Assert;
 
 class EventGridApiClient
 {
@@ -20,18 +18,8 @@ class EventGridApiClient
 
     public function __construct(
         string $topicHostname,
-        ?string $token = null,
         ?ApiClientConfiguration $configuration = null,
     ) {
-        if ($configuration === null) {
-            Assert::stringNotEmpty($token, 'Token must be set when no configuration is provided.');
-            $configuration = new ApiClientConfiguration(
-                authenticator: new CustomHeaderAuth(
-                    header: 'aeg-sas-key',
-                    value: $token,
-                ),
-            );
-        }
         $this->apiClient = new ApiClient(
             sprintf('https://%s', $topicHostname),
             $configuration,

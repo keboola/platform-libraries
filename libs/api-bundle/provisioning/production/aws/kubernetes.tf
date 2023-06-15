b064250cfa598ff8d29f1a465bfeb90c98cd00e3
Pepa Martinec <josef.martinec@keboola.com>
2023-06-15 15:55:01 +0200

    Revert "Production provisioning"
    This reverts commit 15ea27a194dab340c7c09d1eb3d986762aeeb574.

--- libs/api-bundle/provisioning/production/aws/kubernetes.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_service_account" "vault" {
-  metadata {
-    namespace = local.k8s_namespace
-    name      = local.k8s_service_account_name
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.vault.arn
-    }
-  }
-}

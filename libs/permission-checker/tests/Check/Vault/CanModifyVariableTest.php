b6b29ae25d538301e55cfb43ef1d555c2d3e3ab7
Pepa Martinec <josef.martinec@keboola.com>
2023-06-15 21:43:16 +0200

    [permission-checker] SOX-50 Vault permissions check

---
+++ libs/permission-checker/tests/Check/Vault/CanModifyVariableTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\PermissionChecker\Tests\Check\Vault;
+
+use Keboola\PermissionChecker\BranchType;
+use Keboola\PermissionChecker\Check\Vault\CanModifyVariable;
+use Keboola\PermissionChecker\Exception\PermissionDeniedException;
+use Keboola\PermissionChecker\Role;
+use Keboola\PermissionChecker\StorageApiToken;
+use PHPUnit\Framework\TestCase;
+
+class CanModifyVariableTest extends TestCase
+{
+    public static function provideValidPermissionsCheckData(): iterable
+    {
+        yield 'simple token on default branch' => [
+            'branchType' => BranchType::DEFAULT,
+            'token' => new StorageApiToken(),
+        ];
+
+        yield 'simple token on dev branch' => [
+            'branchType' => BranchType::DEV,
+            'token' => new StorageApiToken(),
+        ];
+
+        yield 'developer role on protected dev branch' => [
+            'branchType' => BranchType::DEV,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'developer',
+            ),
+        ];
+
+        yield 'reviewer role on protected dev branch' => [
+            'branchType' => BranchType::DEV,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'reviewer',
+            ),
+        ];
+
+        yield 'productionManager role on protected dev branch' => [
+            'branchType' => BranchType::DEFAULT,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'productionManager',
+            ),
+        ];
+    }
+
+    /** @dataProvider provideValidPermissionsCheckData */
+    public function testValidPermissionsCheck(
+        BranchType $branchType,
+        StorageApiToken $token,
+    ): void {
+        $this->expectNotToPerformAssertions();
+
+        $checker = new CanModifyVariable($branchType);
+        $checker->checkPermissions($token);
+    }
+
+    public static function provideInvalidPermissionsCheckData(): iterable
+    {
+        yield 'readOnly role' => [
+            'branchType' => BranchType::DEFAULT,
+            'token' => new StorageApiToken(
+                role: 'readOnly',
+            ),
+            'error' => PermissionDeniedException::roleDenied(Role::READ_ONLY, 'modify variable'),
+        ];
+
+        yield 'simple token on protected dev branch' => [
+            'branchType' => BranchType::DEV,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+            ),
+            'error' => new PermissionDeniedException(
+                'Role "none" is not allowed to modify variables on dev branch',
+            ),
+        ];
+
+        yield 'developer role on protected default branch' => [
+            'branchType' => BranchType::DEFAULT,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'developer',
+            ),
+            'error' => new PermissionDeniedException(
+                'Role "developer" is not allowed to modify variables on default branch',
+            ),
+        ];
+
+        yield 'reviewer role on protected default branch' => [
+            'branchType' => BranchType::DEFAULT,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'reviewer',
+            ),
+            'error' => new PermissionDeniedException(
+                'Role "reviewer" is not allowed to modify variables on default branch',
+            ),
+        ];
+
+        yield 'productionManager role on protected default branch' => [
+            'branchType' => BranchType::DEV,
+            'token' => new StorageApiToken(
+                features: ['protected-default-branch'],
+                role: 'productionManager',
+            ),
+            'error' => new PermissionDeniedException(
+                'Role "productionManager" is not allowed to modify variables on dev branch',
+            ),
+        ];
+    }
+
+    /** @dataProvider provideInvalidPermissionsCheckData */
+    public function testInvalidPermissionsCheck(
+        BranchType $branchType,
+        StorageApiToken $token,
+        PermissionDeniedException $error,
+    ): void {
+        $this->expectExceptionObject($error);
+
+        $checker = new CanModifyVariable($branchType);
+        $checker->checkPermissions($token);
+    }
+}

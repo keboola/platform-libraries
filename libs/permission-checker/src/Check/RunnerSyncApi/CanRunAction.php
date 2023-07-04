afff8219c71d7c3c2d30313c02b2a8439bc7f62e
Erik Zigo <erik.zigo@keboola.com>
2023-07-04 06:35:21 +0200

    [permission-checker] CanRunAction

---
+++ libs/permission-checker/src/Check/RunnerSyncApi/CanRunAction.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\PermissionChecker\Check\RunnerSyncApi;
+
+use Keboola\PermissionChecker\BranchType;
+use Keboola\PermissionChecker\Exception\PermissionDeniedException;
+use Keboola\PermissionChecker\Feature;
+use Keboola\PermissionChecker\PermissionCheckInterface;
+use Keboola\PermissionChecker\Role;
+use Keboola\PermissionChecker\StorageApiToken;
+
+class CanRunAction implements PermissionCheckInterface
+{
+    public function __construct(
+        private readonly BranchType $branchType,
+        private readonly string $componentId,
+    ) {
+    }
+
+    public function checkPermissions(StorageApiToken $token): void
+    {
+        if (!$token->hasFeature(Feature::QUEUE_V2)) {
+            throw PermissionDeniedException::missingFeature(Feature::QUEUE_V2);
+        }
+
+        if (!$token->hasAllowedComponent($this->componentId)) {
+            throw PermissionDeniedException::missingComponent($this->componentId);
+        }
+
+        if ($token->hasFeature(Feature::PROTECTED_DEFAULT_BRANCH)) {
+            $this->checkProtectedDefaultBranch($token->getRole());
+        } elseif ($token->isRole(Role::READ_ONLY)) {
+            throw PermissionDeniedException::roleDenied($token->getRole(), 'run actions');
+        }
+    }
+
+    private function checkProtectedDefaultBranch(Role $role): void
+    {
+        $isAllowed = match ($role) {
+            Role::PRODUCTION_MANAGER => $this->branchType === BranchType::DEFAULT,
+            Role::DEVELOPER, Role::REVIEWER => $this->branchType !== BranchType::DEFAULT,
+            default => false,
+        };
+
+        if (!$isAllowed) {
+            throw new PermissionDeniedException(sprintf(
+                'Role "%s" is not allowed to run actions on %s branch',
+                $role->value,
+                $this->branchType->value,
+            ));
+        }
+    }
+}
---
+++ libs/permission-checker/tests/Check/RunnerSyncApi/CanRunActionTest.php
@@ -0,0 +1,200 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\PersmissionChecker\Tests\Check\RunnerSyncApi;
+
+use Generator;
+use Keboola\PermissionChecker\BranchType;
+use Keboola\PermissionChecker\Check\RunnerSyncApi\CanRunAction;
+use Keboola\PermissionChecker\Exception\PermissionDeniedException;
+use Keboola\PermissionChecker\Feature;
+use Keboola\PermissionChecker\StorageApiToken;
+use PHPUnit\Framework\TestCase;
+
+class CanRunActionTest extends TestCase
+{
+    public function branchTypeProvider(): Generator
+    {
+        yield 'dev branch' => [BranchType::DEV];
+        yield 'default branch' => [BranchType::DEFAULT];
+    }
+
+    public static function provideValidPermissionsCheckData(): Generator
+    {
+        /** @var (BranchType)[] $branchTypes */
+        $branchTypes = [BranchType::DEFAULT, BranchType::DEV];
+        $roles = [null, 'guest', 'readOnly', 'admin', 'share', 'developer', 'reviewer', 'productionManager'];
+        $allowedComponentsOptions = [null, ['dummy-component']];
+
+        // standard projects
+        foreach ($branchTypes as $branchType) {
+            foreach ($roles as $role) {
+                if ($role === 'readOnly') {
+                    continue;
+                }
+
+                $label = $role ?: 'regular token';
+                $label .= sprintf(' on %s branch', $branchType->value);
+
+                foreach ($allowedComponentsOptions as $allowedComponents) {
+                    $providedDataLabel = $label;
+                    $providedDataLabel .= $allowedComponents ? ' with all compoment access'
+                        : ' with dummy-component access';
+                    yield $providedDataLabel => [
+                        'token' => new StorageApiToken(
+                            role: $role,
+                            features: ['queuev2'],
+                            allowedComponents: $allowedComponents
+                        ),
+                        'branchType' => $branchType,
+                    ];
+                }
+            }
+        }
+
+        // sox projects
+        yield 'sox productionManager role on default branch' => [
+            'token' => new StorageApiToken(
+                role: 'productionManager',
+                features: [
+                    'protected-default-branch',
+                    'queuev2',
+                ]
+            ),
+            'branchType' => BranchType::DEFAULT,
+        ];
+
+        yield 'sox developer role on dev branch' => [
+            'token' => new StorageApiToken(
+                role: 'developer',
+                features: [
+                    'protected-default-branch',
+                    'queuev2',
+                ]
+            ),
+            'branchType' => BranchType::DEV,
+        ];
+
+        yield 'sox reviewer role on dev branch' => [
+            'token' => new StorageApiToken(
+                role: 'reviewer',
+                features: [
+                    'protected-default-branch',
+                    'queuev2',
+                ]
+            ),
+            'branchType' => BranchType::DEV,
+        ];
+    }
+
+    /** @dataProvider provideValidPermissionsCheckData */
+    public function testValidPermissionsCheck(
+        StorageApiToken $token,
+        BranchType $branchType
+    ): void {
+        $this->expectNotToPerformAssertions();
+
+        $checker = new CanRunAction($branchType, 'dummy-component');
+        $checker->checkPermissions($token);
+    }
+
+    public static function provideInvalidPermissionsCheckData(): Generator
+    {
+        $branchTypes = [BranchType::DEFAULT, BranchType::DEV];
+
+        foreach ($branchTypes as $branchType) {
+            $label = 'project without new queue';
+            $label .= sprintf(' on %s branch', $branchType->value);
+
+            yield $label => [
+                'token' => new StorageApiToken(),
+                'branchType' => $branchType,
+                'errorMessage' => 'Project does not have feature "queuev2" enabled',
+            ];
+
+            $label = 'regular token without required component access';
+            $label .= sprintf(' on %s branch', $branchType->value);
+
+            yield $label => [
+                'token' => new StorageApiToken(
+                    features: ['queuev2'],
+                    allowedComponents: ['keboola.orchestrator']
+                ),
+                'branchType' => $branchType,
+                'errorMessage' => 'Token is not allowed to run component "dummy-component"',
+            ];
+
+            $label = 'regular token without any component access';
+            $label .= sprintf(' on %s branch', $branchType->value);
+
+            yield $label => [
+                'token' => new StorageApiToken(
+                    features: ['queuev2'],
+                    allowedComponents: []
+                ),
+                'branchType' => $branchType,
+                'errorMessage' => 'Token is not allowed to run component "dummy-component"',
+            ];
+
+            $label = 'readOnly';
+            $label .= sprintf(' on %s branch', $branchType->value);
+
+            yield $label => [
+                'token' => new StorageApiToken(
+                    role: 'readOnly',
+                    features: ['queuev2'],
+                ),
+                'branchType' => $branchType,
+                'errorMessage' => 'Role "readOnly" is not allowed to run actions',
+            ];
+        }
+
+        // sox projects
+        $roles = [null, 'guest', 'readOnly', 'admin', 'share', 'developer', 'reviewer', 'productionManager'];
+
+        foreach ($branchTypes as $branchType) {
+            foreach ($roles as $role) {
+                if ($role === 'productionManager' && $branchType === BranchType::DEFAULT) {
+                    continue;
+                }
+                if (in_array($role, ['developer', 'reviewer']) && $branchType === BranchType::DEV) {
+                    continue;
+                }
+
+                $label = 'sox ';
+                $label .= $role ?: 'regular token';
+                $label .= sprintf(' on %s branch', $branchType->value);
+
+                yield $label => [
+                    'token' => new StorageApiToken(
+                        role: $role,
+                        features: [
+                            'queuev2',
+                            'protected-default-branch',
+                        ]
+                    ),
+                    'branchType' => $branchType,
+                    'errorMessage' => sprintf(
+                        'Role "%s" is not allowed to run actions on %s branch',
+                        $role?: 'none',
+                        $branchType->value
+                    ),
+                ];
+            }
+        }
+    }
+
+    /** @dataProvider provideInvalidPermissionsCheckData */
+    public function testInvalidPermissionsCheck(
+        StorageApiToken $token,
+        BranchType $branchType,
+        string $errorMessage,
+    ): void {
+        $this->expectException(PermissionDeniedException::class);
+        $this->expectExceptionMessage($errorMessage);
+
+        $checker = new CanRunAction($branchType, 'dummy-component');
+        $checker->checkPermissions($token);
+    }
+}

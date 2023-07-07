46917b6cc83654ff30fe13c6376eb436810a0ead
Erik Zigo <erik.zigo@keboola.com>
2023-07-07 14:30:16 +0200

    [permission-checker] CanRunActionTest fixup

--- libs/permission-checker/tests/Check/RunnerSyncApi/CanRunActionTest.php
+++ libs/permission-checker/tests/Check/RunnerSyncApi/CanRunActionTest.php
@@ -8,6 +8,8 @@ use Generator;
 use Keboola\PermissionChecker\BranchType;
 use Keboola\PermissionChecker\Check\RunnerSyncApi\CanRunAction;
 use Keboola\PermissionChecker\Exception\PermissionDeniedException;
+use Keboola\PermissionChecker\Feature;
+use Keboola\PermissionChecker\Role;
 use Keboola\PermissionChecker\StorageApiToken;
 use PHPUnit\Framework\TestCase;
 
@@ -15,151 +17,79 @@ class CanRunActionTest extends TestCase
 {
     public function branchTypeProvider(): Generator
     {
-        yield 'dev branch' => [BranchType::DEV];
-        yield 'default branch' => [BranchType::DEFAULT];
-    }
-
-    public static function provideValidPermissionsCheckData(): Generator
-    {
-        /** @var (BranchType)[] $branchTypes */
-        $branchTypes = [BranchType::DEFAULT, BranchType::DEV];
-        $roles = [null, 'guest', 'readOnly', 'admin', 'share', 'developer', 'reviewer', 'productionManager'];
-        $allowedComponentsOptions = [null, ['dummy-component']];
-
-        // standard projects
-        foreach ($branchTypes as $branchType) {
-            foreach ($roles as $role) {
-                if ($role === 'readOnly') {
-                    continue;
-                }
-
-                $label = $role ?: 'regular token';
-                $label .= sprintf(' on %s branch', $branchType->value);
-
-                foreach ($allowedComponentsOptions as $allowedComponents) {
-                    $providedDataLabel = $label;
-                    $providedDataLabel .= $allowedComponents ? ' with all compoment access'
-                        : ' with dummy-component access';
-                    yield $providedDataLabel => [
-                        'token' => new StorageApiToken(
-                            role: $role,
-                            allowedComponents: $allowedComponents
-                        ),
-                        'branchType' => $branchType,
-                    ];
-                }
-            }
+        foreach (BranchType::cases() as $branchType) {
+            yield $branchType->value => [$branchType];
         }
-
-        // sox projects
-        yield 'sox productionManager role on default branch' => [
-            'token' => new StorageApiToken(
-                features: [
-                    'protected-default-branch',
-                ],
-                role: 'productionManager'
-            ),
-            'branchType' => BranchType::DEFAULT,
-        ];
-
-        yield 'sox developer role on dev branch' => [
-            'token' => new StorageApiToken(
-                features: [
-                    'protected-default-branch',
-                ],
-                role: 'developer'
-            ),
-            'branchType' => BranchType::DEV,
-        ];
-
-        yield 'sox reviewer role on dev branch' => [
-            'token' => new StorageApiToken(
-                features: [
-                    'protected-default-branch',
-                ],
-                role: 'reviewer'
-            ),
-            'branchType' => BranchType::DEV,
-        ];
     }
 
-    /** @dataProvider provideValidPermissionsCheckData */
-    public function testValidPermissionsCheck(
-        StorageApiToken $token,
-        BranchType $branchType
-    ): void {
-        $this->expectNotToPerformAssertions();
+    /**
+     * @dataProvider branchTypeProvider
+     */
+    public function testPermissionCheckForTokensWithoutPermissionToComponent(BranchType $branchType): void
+    {
+        $tokenMock = $this->createMock(StorageApiToken::class);
+        $tokenMock->expects(self::once())
+            ->method('hasAllowedComponent')
+            ->with('dummy-component')
+            ->willReturn(false)
+        ;
+
+        $this->expectException(PermissionDeniedException::class);
+        $this->expectExceptionMessage('Token is not allowed to run component "dummy-component"');
 
         $checker = new CanRunAction($branchType, 'dummy-component');
-        $checker->checkPermissions($token);
+        $checker->checkPermissions($tokenMock);
     }
 
-    public static function provideInvalidPermissionsCheckData(): Generator
+    /**
+     * @dataProvider branchTypeProvider
+     */
+    public function testPermissionCheckFailOnReadOnlyRole(BranchType $branchType): void
     {
-        $branchTypes = [BranchType::DEFAULT, BranchType::DEV];
+        $tokenMock = $this->createMock(StorageApiToken::class);
+        $tokenMock->expects(self::once())
+            ->method('hasAllowedComponent')
+            ->with('dummy-component')
+            ->willReturn(true)
+        ;
 
-        foreach ($branchTypes as $branchType) {
-            $label = 'regular token without required component access';
-            $label .= sprintf(' on %s branch', $branchType->value);
+        $tokenMock->expects(self::once())
+            ->method('hasFeature')
+            ->with(Feature::PROTECTED_DEFAULT_BRANCH)
+            ->willReturn(false)
+        ;
 
-            yield $label => [
-                'token' => new StorageApiToken(
-                    allowedComponents: ['keboola.orchestrator']
-                ),
-                'branchType' => $branchType,
-                'errorMessage' => 'Token is not allowed to run component "dummy-component"',
-            ];
+        $tokenMock->expects(self::once())
+            ->method('isRole')
+            ->with(Role::READ_ONLY)
+            ->willReturn(true)
+        ;
 
-            $label = 'regular token without any component access';
-            $label .= sprintf(' on %s branch', $branchType->value);
+        $tokenMock->expects(self::once())
+            ->method('getRole')
+            ->willReturn(Role::READ_ONLY)
+        ;
 
-            yield $label => [
-                'token' => new StorageApiToken(
-                    allowedComponents: []
-                ),
-                'branchType' => $branchType,
-                'errorMessage' => 'Token is not allowed to run component "dummy-component"',
-            ];
+        $this->expectException(PermissionDeniedException::class);
+        $this->expectExceptionMessage('Role "readOnly" is not allowed to run actions');
 
-            $label = 'readOnly';
-            $label .= sprintf(' on %s branch', $branchType->value);
+        $checker = new CanRunAction($branchType, 'dummy-component');
+        $checker->checkPermissions($tokenMock);
+    }
 
-            yield $label => [
-                'token' => new StorageApiToken(
-                    role: 'readOnly',
-                ),
-                'branchType' => $branchType,
-                'errorMessage' => 'Role "readOnly" is not allowed to run actions',
-            ];
-        }
-
-        // sox projects
-        $roles = [null, 'guest', 'readOnly', 'admin', 'share', 'developer', 'reviewer', 'productionManager'];
-
-        foreach ($branchTypes as $branchType) {
-            foreach ($roles as $role) {
-                if ($role === 'productionManager' && $branchType === BranchType::DEFAULT) {
+    public function validPermissionsCheckProvider(): Generator
+    {
+        foreach (BranchType::cases() as $branchType) {
+            foreach (Role::cases() as $role) {
+                if ($role === Role::READ_ONLY) {
                     continue;
                 }
-                if (in_array($role, ['developer', 'reviewer']) && $branchType === BranchType::DEV) {
-                    continue;
-                }
-
-                $label = 'sox ';
-                $label .= $role ?: 'regular token';
-                $label .= sprintf(' on %s branch', $branchType->value);
-
-                yield $label => [
-                    'token' => new StorageApiToken(
-                        features: [
-                            'protected-default-branch',
-                        ],
-                        role: $role
-                    ),
+                yield $branchType->value . ' branch, ' . $role->value => [
+                    'role' => $role,
                     'branchType' => $branchType,
-                    'errorMessage' => sprintf(
+                    'expectedErrorMessage' => sprintf(
                         'Role "%s" is not allowed to run actions on %s branch',
-                        $role?: 'none',
+                        $role->value,
                         $branchType->value
                     ),
                 ];
@@ -167,16 +97,128 @@ class CanRunActionTest extends TestCase
         }
     }
 
-    /** @dataProvider provideInvalidPermissionsCheckData */
-    public function testInvalidPermissionsCheck(
-        StorageApiToken $token,
-        BranchType $branchType,
-        string $errorMessage,
-    ): void {
-        $this->expectException(PermissionDeniedException::class);
-        $this->expectExceptionMessage($errorMessage);
+    /**
+     * @dataProvider validPermissionsCheckProvider
+     */
+    public function testValidPermissionsCheck(Role $role, BranchType $branchType): void
+    {
+        $tokenMock = $this->createMock(StorageApiToken::class);
+        $tokenMock->expects(self::once())
+            ->method('hasAllowedComponent')
+            ->with('dummy-component')
+            ->willReturn(true)
+        ;
+
+        $tokenMock->expects(self::once())
+            ->method('hasFeature')
+            ->with(Feature::PROTECTED_DEFAULT_BRANCH)
+            ->willReturn(false)
+        ;
 
         $checker = new CanRunAction($branchType, 'dummy-component');
-        $checker->checkPermissions($token);
+        $checker->checkPermissions($tokenMock);
+    }
+
+    public function permissionCheckFailOnSoxProjectsProvider(): Generator
+    {
+        foreach (BranchType::cases() as $branchType) {
+            foreach (Role::cases() as $role) {
+                if ($role === Role::PRODUCTION_MANAGER && $branchType === BranchType::DEFAULT) {
+                    continue;
+                }
+                if ($role === Role::DEVELOPER && $branchType === BranchType::DEV) {
+                    continue;
+                }
+                if ($role === Role::REVIEWER && $branchType === BranchType::DEV) {
+                    continue;
+                }
+
+                yield $branchType->value . ' branch, ' . $role->value => [
+                    'role' => $role,
+                    'branchType' => $branchType,
+                    'expectedErrorMessage' => sprintf(
+                        'Role "%s" is not allowed to run actions on %s branch',
+                        $role->value,
+                        $branchType->value
+                    ),
+                ];
+            }
+        }
+    }
+
+    /**
+     * @dataProvider permissionCheckFailOnSoxProjectsProvider
+     */
+    public function testPermissionCheckFailOnSoxProjects(
+        Role $role,
+        BranchType $branchType,
+        string $expectedErrorMessage
+    ): void {
+        $tokenMock = $this->createMock(StorageApiToken::class);
+        $tokenMock->expects(self::once())
+            ->method('hasAllowedComponent')
+            ->with('dummy-component')
+            ->willReturn(true)
+        ;
+
+        $tokenMock->expects(self::once())
+            ->method('hasFeature')
+            ->with(Feature::PROTECTED_DEFAULT_BRANCH)
+            ->willReturn(true)
+        ;
+
+        $tokenMock->expects(self::once())
+            ->method('getRole')
+            ->willReturn($role)
+        ;
+
+        $this->expectException(PermissionDeniedException::class);
+        $this->expectExceptionMessage($expectedErrorMessage);
+
+        $checker = new CanRunAction($branchType, 'dummy-component');
+        $checker->checkPermissions($tokenMock);
+    }
+
+    public function validPermissionsCheckOnSoxProjectsProvider(): Generator
+    {
+        yield 'default branch, productionManager' => [
+            'role' => Role::PRODUCTION_MANAGER,
+            'branchType' => BranchType::DEFAULT,
+        ];
+        yield 'dev branch, developer' => [
+            'role' => Role::DEVELOPER,
+            'branchType' => BranchType::DEV,
+        ];
+        yield 'dev branch, reviewer' => [
+            'role' => Role::REVIEWER,
+            'branchType' => BranchType::DEV,
+        ];
+    }
+
+    /**
+     * @dataProvider validPermissionsCheckOnSoxProjectsProvider
+     */
+    public function testValidPermissionsCheckOnSoxProjects(Role $role, BranchType $branchType): void
+    {
+        $tokenMock = $this->createMock(StorageApiToken::class);
+        $tokenMock->expects(self::once())
+            ->method('hasAllowedComponent')
+            ->with('dummy-component')
+            ->willReturn(true)
+        ;
+
+        $tokenMock->expects(self::once())
+            ->method('hasFeature')
+            ->with(Feature::PROTECTED_DEFAULT_BRANCH)
+            ->willReturn(true)
+        ;
+
+        $tokenMock->expects(self::once())
+            ->method('getRole')
+            ->willReturn($role)
+        ;
+
+        $checker = new CanRunAction($branchType, 'dummy-component');
+        $checker->checkPermissions($tokenMock);
     }
 }

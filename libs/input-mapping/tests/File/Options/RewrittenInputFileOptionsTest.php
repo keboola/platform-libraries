f6e3795a62666d2ba8317d9ea7f9733854ecfbd6
odin <odinuv@gmail.com>
2023-07-18 16:32:06 +0200

    add tests

--- libs/input-mapping/tests/File/Options/RewrittenInputFileOptionsTest.php
+++ libs/input-mapping/tests/File/Options/RewrittenInputFileOptionsTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Keboola\InputMapping\Tests\File\Options;
 
+use Generator;
 use Keboola\InputMapping\File\Options\RewrittenInputFileOptions;
+use Keboola\InputMapping\State\InputFileStateList;
+use Keboola\StorageApi\Options\ListFilesOptions;
 use PHPUnit\Framework\TestCase;
 
 class RewrittenInputFileOptionsTest extends TestCase
@@ -32,5 +35,146 @@ class RewrittenInputFileOptionsTest extends TestCase
             [['name' => 'bar']],
             $options->getFileConfigurationIdentifier()
         );
+    }
+
+    /** @dataProvider provideFileListOptionsValues */
+    public function testGetStorageApiFileListOptions(
+        array $rewrittenFileOptions,
+        bool $isDevBranch,
+        string $runId,
+        InputFileStateList $fileStates,
+        ListFilesOptions $expectedOptions,
+        ?array $originalFileOptions,
+    ): void
+    {
+        if ($originalFileOptions === null) {
+            $originalFileOptions = $rewrittenFileOptions;
+        }
+        $inputFileOptions = new RewrittenInputFileOptions(
+            $rewrittenFileOptions,
+            $isDevBranch,
+            $runId,
+            $originalFileOptions,
+        );
+        $options = $inputFileOptions->getStorageApiFileListOptions($fileStates);
+        self::assertEquals($expectedOptions, $options);
+    }
+
+    public function provideFileListOptionsValues(): Generator
+    {
+        yield 'tags' => [
+            'rewrittenFileOptions' => ['tags' => ['foo']],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo'])->setLimit(100),
+            'originalFileOptions' => null,
+        ];
+        yield 'tags + filter by run id + limit' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'filter_by_run_id' => true, 'limit' => 50],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo'])->setRunId(1234)->setLimit(50),
+            'originalFileOptions' => null,
+        ];
+        yield 'source tags include' => [
+            'rewrittenFileOptions' => ['source' =>
+                ['tags' => [['name' => 'foo', 'match' => 'include']]],
+                'limit' => 50,
+            ],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setQuery('tags:"foo"')->setLimit(50),
+            'originalFileOptions' => null,
+        ];
+        yield 'source tags exclude' => [
+            'rewrittenFileOptions' => ['source' => ['tags' => [['name' => 'foo', 'match' => 'exclude']]]],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setQuery('NOT tags:"foo"')->setLimit(100),
+            'originalFileOptions' => null,
+        ];
+        yield 'query' => [
+            'rewrittenFileOptions' => ['query' => 'foo bar'],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setQuery('foo bar'),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since + tags' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'changed_since' => '2023-07-18T12:00:00+00:00'],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo'])
+                ->setQuery('created:["2023-07-18T12:00:00+00:00" TO *]'),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since adaptive + tags + no state' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'changed_since' => 'adaptive'],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo']),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since adaptive + tags + some state' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'changed_since' => 'adaptive'],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([
+                [
+                    'tags' => [['name' => 'foo']],
+                    'lastImportId' => '1234567',
+                ],
+            ]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo'])->setSinceId('1234567'),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since adaptive + tags + invalid state' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'changed_since' => 'adaptive'],
+            'isDevBranch' => false,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([
+                [
+                    'tags' => [['name' => 'bar']],
+                    'lastImportId' => '1234567',
+                ],
+            ]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo']),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since adaptive + tags + state + branch' => [
+            'rewrittenFileOptions' => ['tags' => ['foo'], 'changed_since' => 'adaptive'],
+            'isDevBranch' => true,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([
+                [
+                    'tags' => [['name' => 'foo']],
+                    'lastImportId' => '1234567',
+                ],
+            ]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['foo'])->setSinceId('1234567'),
+            'originalFileOptions' => null,
+        ];
+        yield 'changed_since adaptive + tags + state + branch + rewritten tags' => [
+            'rewrittenFileOptions' => ['tags' => ['1234-foo'], 'changed_since' => 'adaptive'],
+            'isDevBranch' => true,
+            'runId' => '1234',
+            'filesStates' => new InputFileStateList([
+                [
+                    /* even that the tag is 1234-foo, the state from foo is still used, frankly I have no idea why
+                        the state is not rewritten as well, but that it how it works now */
+                    'tags' => [['name' => 'foo']],
+                    'lastImportId' => '1234567',
+                ],
+            ]),
+            'expectedOptions' => (new ListFilesOptions())->setTags(['1234-foo'])->setSinceId('1234567'),
+            'originalFileOptions' => ['tags' => ['foo']],
+        ];
     }
 }

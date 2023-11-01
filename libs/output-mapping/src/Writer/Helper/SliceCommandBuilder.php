b6e00e347be7fd2e5a0f55279adae7a38c165c87
Erik Zigo <erik.zigo@keboola.com>
2023-11-01 19:04:54 +0100

    [output-mapping] SliceCommandBuilder - output dir param

--- libs/output-mapping/src/Writer/Helper/SliceCommandBuilder.php
+++ libs/output-mapping/src/Writer/Helper/SliceCommandBuilder.php
@@ -8,20 +8,23 @@ use SplFileInfo;
 
 class SliceCommandBuilder
 {
-    public static function create(SplFileInfo $souceFile, ?SplFileInfo $manifestFile = null): array
-    {
+    public static function create(
+        SplFileInfo $inputFile,
+        SplFileInfo $outputDir,
+        ?SplFileInfo $inputManifestFile = null,
+    ): array {
         $command = [
             './bin/slicer',
-            '--table-input-path=' . $souceFile->getPathname(),
+            '--table-input-path=' . $inputFile->getPathname(),
             '--table-name=LOG_PLACEHOLDER', // @TODO optional? we do not know at this moment the table name
-            '--table-output-path=' . $souceFile->getPathname() . 'sliced',
-            '--table-output-manifest-path=' . $souceFile->getPathname() . 'sliced.manifest',
-            '--gzip=false', // for tests purpose
+            '--table-output-path=' . $outputDir->getPathname(),
+            '--table-output-manifest-path=' . $outputDir->getPathname() . '.manifest',
+            '--gzip=false', // for tests purpose, gzip will be implemented in future
         ];
 
-        if ($manifestFile && $manifestFile->isFile()) {
+        if ($inputManifestFile && $inputManifestFile->isFile()) {
             // @TODO if manifest does not exists, the slicer does not fail
-            $command[] = '--table-input-manifest-path=' . $manifestFile->getPathname();
+            $command[] = '--table-input-manifest-path=' . $inputManifestFile->getPathname();
         }
 
         return $command;

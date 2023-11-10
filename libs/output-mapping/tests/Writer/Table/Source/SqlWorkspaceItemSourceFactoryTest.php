c3fafe0769fe53f3b9b314e3ebbe46d47b870c9e
Erik Zigo <erik.zigo@keboola.com>
2023-11-10 15:33:55 +0100

    [output-mapping] WorkspaceItemSourceFactory tests

---
+++ libs/output-mapping/tests/Writer/Table/Source/SqlWorkspaceItemSourceFactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\OutputMapping\Tests\Writer\Table\Source;
+
+use Keboola\InputMapping\Staging\ProviderInterface;
+use Keboola\OutputMapping\Writer\Table\Source\SqlWorkspaceItemSourceFactory;
+use PHPUnit\Framework\TestCase;
+
+class SqlWorkspaceItemSourceFactoryTest extends TestCase
+{
+    public function testCreateSource(): void
+    {
+        $stagingProviderMock = $this->createMock(ProviderInterface::class);
+        $stagingProviderMock->expects(self::once())
+            ->method('getWorkspaceId')
+            ->willReturn('123456')
+        ;
+
+        $factory = new SqlWorkspaceItemSourceFactory($stagingProviderMock);
+        $workspaceItemSource = $factory->createSource('upload', 'myName');
+
+        self::assertSame('123456', $workspaceItemSource->getWorkspaceId());
+        self::assertSame('myName', $workspaceItemSource->getName());
+        self::assertSame('myName', $workspaceItemSource->getDataObject());
+        self::assertFalse($workspaceItemSource->isSliced());
+    }
+}

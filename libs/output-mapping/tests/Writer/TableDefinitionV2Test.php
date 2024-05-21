4117e0b435f00d57078004c41779d2d140cb5a89
Ondřej Jodas <ondra.jodas@gmail.com>
2024-05-21 10:42:09 +0200

    detect conflicts between old config and new manifest

--- libs/output-mapping/tests/Writer/TableDefinitionV2Test.php
+++ libs/output-mapping/tests/Writer/TableDefinitionV2Test.php
@@ -36,8 +36,114 @@ class TableDefinitionV2Test extends AbstractTestCase
         }
     }
 
-    public function testTemporaryDeleteMe(): void
+    /**
+     * @dataProvider conflictsConfigurationWithManifestProvider
+     */
+    public function testConflictSchemaManifestAndNonSchemaConfiguration(
+        array $config,
+        string $expectedErrorMessage,
+    ): void {
+        $root = $this->temp->getTmpFolder();
+        file_put_contents(
+            $root . '/upload/tableDefinition.csv',
+            "\"Id\",\"Name\"\n\"test\",\"test\"\n\"aabb\",\"ccdd\"\n",
+        );
+
+        file_put_contents(
+            $root . '/upload/tableDefinition.csv.manifest',
+            json_encode([
+                'schema' => [
+                    [
+                        'name' => 'Id',
+                        'primary_key' => true,
+                        'data_type' => [
+                            'base' => [
+                                'type' => 'int',
+                            ],
+                        ],
+                    ],
+                    [
+                        'name' => 'Name',
+                        'data_type' => [
+                            'base' => [
+                                'type' => 'string',
+                            ],
+                        ],
+                    ],
+                ],
+            ]),
+        );
+
+        $baseConfig = [
+            'source' => 'tableDefinition.csv',
+            'destination' => 'in.c-test.tableDefinition',
+        ];
+
+        $writer = new TableWriter($this->getLocalStagingFactory(logger: $this->testLogger));
+
+        $this->expectException(InvalidOutputException::class);
+        $this->expectExceptionMessage($expectedErrorMessage);
+        $writer->uploadTables(
+            'upload',
+            ['mapping' => [array_merge($baseConfig, $config)]],
+            ['componentId' => 'foo'],
+            'local',
+            false,
+            'none',
+        );
+    }
+
+    public function conflictsConfigurationWithManifestProvider(): Generator
     {
-        self::assertTrue(true);
+        yield 'conflict-columns' => [
+            [
+                'columns' => ['Id', 'Name'],
+            ],
+            'Only one of "schema" or "columns" can be defined.',
+        ];
+
+        yield 'conflict-primary_keys' => [
+            [
+                'primary_key' => ['Id', 'Name'],
+            ],
+            'Only one of "primary_key" or "schema[].primary_key" can be defined.',
+        ];
+
+        yield 'conflict-distribution_key' => [
+            [
+                'distribution_key' => ['Id', 'Name'],
+            ],
+            'Only one of "distribution_key" or "schema[].distribution_key" can be defined.',
+        ];
+
+        yield 'conflict-metadata' => [
+            [
+                'metadata' => [
+                    [
+                        'key' => 'table.key.one',
+                        'value' => 'table value one',
+                    ],
+                    [
+                        'key' => 'table.key.two',
+                        'value' => 'table value two',
+                    ],
+                ],
+            ],
+            'Only one of "schema" or "metadata" can be defined.',
+        ];
+
+        yield 'conflict-column_metadata' => [
+            [
+                'column_metadata' => [
+                    'Id' => [
+                        [
+                            'key' => 'KBC.dataType',
+                            'value' => 'VARCHAR',
+                        ],
+                    ],
+                ],
+            ],
+            'Only one of "schema" or "column_metadata" can be defined.',
+        ];
     }
 }

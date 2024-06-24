8386b13d37d5138025eb9f1442678315ebc23bce
Ondřej Jodas <ondra.jodas@gmail.com>
2024-06-24 23:15:22 +0200

    schema column metadata applier

---
+++ libs/output-mapping/src/DeferredTasks/Metadata/SchemaColumnMetadata.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\OutputMapping\DeferredTasks\Metadata;
+
+use Keboola\OutputMapping\Mapping\MappingFromConfigurationSchemaColumn;
+use Keboola\StorageApi\Metadata;
+use Keboola\StorageApi\Options\Metadata\TableMetadataUpdateOptions;
+use Keboola\Utils\Sanitizer\ColumnNameSanitizer;
+
+class SchemaColumnMetadata implements MetadataInterface
+{
+    /** @param MappingFromConfigurationSchemaColumn[] $schema */
+    public function __construct(
+        private readonly string $tableId,
+        private readonly string $provider,
+        private readonly array $schema,
+    ) {
+    }
+
+    public function apply(Metadata $apiClient, int $bulkSize = 100): void
+    {
+        assert($bulkSize > 0);
+
+        /** @var MappingFromConfigurationSchemaColumn[] $chunk */
+        foreach (array_chunk($this->schema, $bulkSize, true) as $chunk) {
+            $columnsMetadata = [];
+
+            foreach ($chunk as $metadataArray) {
+                $columnMetadata = [];
+                $columnName = ColumnNameSanitizer::sanitize($metadataArray->getName());
+                foreach ($metadataArray->getMetadata() as $key => $value) {
+                    $columnMetadata[] = [
+                        'columnName' => $columnName,
+                        'key' => (string) $key,
+                        'value' => (string) $value,
+                    ];
+                }
+
+                $columnsMetadata[$columnName] = $columnMetadata;
+            }
+
+            $options = new TableMetadataUpdateOptions(
+                $this->tableId,
+                $this->provider,
+                null,
+                $columnsMetadata,
+            );
+
+            $apiClient->postTableMetadataWithColumns($options);
+        }
+    }
+}

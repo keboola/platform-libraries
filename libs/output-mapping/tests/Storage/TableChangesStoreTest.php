d214fbac506d98ae00ac3f8c905a1daa22a74c93
Erik Zigo <erik.zigo@keboola.com>
2024-07-04 17:24:12 +0200

    output-mapping: TableChangesStoreTest

---
+++ libs/output-mapping/tests/Storage/TableChangesStoreTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\OutputMapping\Tests\Storage;
+
+use Keboola\OutputMapping\Mapping\MappingFromConfigurationSchemaColumn;
+use Keboola\OutputMapping\Storage\TableChangesStore;
+use PHPUnit\Framework\TestCase;
+
+class TableChangesStoreTest extends TestCase
+{
+    public function testAccessors(): void
+    {
+        $changesStore = new TableChangesStore();
+
+        self::assertFalse($changesStore->hasMissingColumns());
+        self::assertFalse($changesStore->hasChanges());
+        self::assertSame([], $changesStore->getMissingColumns());
+
+        $changesStore->addMissingColumn(new MappingFromConfigurationSchemaColumn([
+            'name' => 'newColumn',
+            'data_type' => [
+                'base' => [
+                    'type' => 'STRING',
+                    'length' => '255',
+                ],
+                'snowflake' => [
+                    'type' => 'VARCHAR',
+                ],
+            ],
+        ]));
+    }
+
+    public function testAddMissingColumn(): void
+    {
+        $expectedSchemaColumn = new MappingFromConfigurationSchemaColumn([
+            'name' => 'newColumn',
+            'data_type' => [
+                'base' => [
+                    'type' => 'STRING',
+                    'length' => '255',
+                ],
+                'snowflake' => [
+                    'type' => 'VARCHAR',
+                ],
+            ],
+        ]);
+
+        $changesStore = new TableChangesStore();
+        $changesStore->addMissingColumn($expectedSchemaColumn);
+
+        self::assertTrue($changesStore->hasMissingColumns());
+        self::assertTrue($changesStore->hasChanges());
+        self::assertSame([$expectedSchemaColumn], $changesStore->getMissingColumns());
+    }
+}

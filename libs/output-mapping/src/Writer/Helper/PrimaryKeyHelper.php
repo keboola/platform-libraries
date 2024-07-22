d64308391738d7e5915e3ca88b1278f771d4cb57
Erik Zigo <erik.zigo@keboola.com>
2024-07-22 11:12:04 +0200

    Revert "output-mapping: modifyPrimaryKeyDecider + modifyPrimaryKey"
    This reverts commit 05ed99b4959a073025877d884c0865525216acde.

--- libs/output-mapping/src/Writer/Helper/PrimaryKeyHelper.php
+++ libs/output-mapping/src/Writer/Helper/PrimaryKeyHelper.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Keboola\OutputMapping\Writer\Helper;
 
+use Keboola\StorageApi\Client;
 use Psr\Log\LoggerInterface;
+use Throwable;
 
 class PrimaryKeyHelper
 {
@@ -29,5 +31,72 @@ class PrimaryKeyHelper
                 }),
             ),
         );
+    }
+
+    public static function modifyPrimaryKeyDecider(
+        LoggerInterface $logger,
+        array $currentTablePrimaryKey,
+        array $newTableConfigurationPrimaryKey,
+    ): bool {
+        $configPK = self::normalizeKeyArray($logger, $newTableConfigurationPrimaryKey);
+        if (count($currentTablePrimaryKey) !== count($configPK)) {
+            return true;
+        }
+        $currentTablePkColumnsCount = count($currentTablePrimaryKey);
+        if (count(array_intersect($currentTablePrimaryKey, $configPK)) !== $currentTablePkColumnsCount) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function modifyPrimaryKey(
+        LoggerInterface $logger,
+        Client $client,
+        string $tableId,
+        array $tablePrimaryKey,
+        array $configPrimaryKey,
+    ): void {
+        $logger->warning(sprintf(
+            'Modifying primary key of table "%s" from "%s" to "%s".',
+            $tableId,
+            join(', ', $tablePrimaryKey),
+            join(', ', $configPrimaryKey),
+        ));
+        if (self::removePrimaryKey($logger, $client, $tableId, $tablePrimaryKey)) {
+            // modify primary key
+            try {
+                if (count($configPrimaryKey)) {
+                    $client->createTablePrimaryKey($tableId, $configPrimaryKey);
+                }
+            } catch (Throwable $e) {
+                // warn and try to rollback to original state
+                $logger->warning(
+                    "Error changing primary key of table {$tableId}: " . $e->getMessage(),
+                );
+                if (count($tablePrimaryKey) > 0) {
+                    $client->createTablePrimaryKey($tableId, $tablePrimaryKey);
+                }
+            }
+        }
+    }
+
+    private static function removePrimaryKey(
+        LoggerInterface $logger,
+        Client $client,
+        string $tableId,
+        array $tablePrimaryKey,
+    ): bool {
+        if (count($tablePrimaryKey) > 0) {
+            try {
+                $client->removeTablePrimaryKey($tableId);
+            } catch (Throwable $e) {
+                // warn and go on
+                $logger->warning(
+                    "Error deleting primary key of table {$tableId}: " . $e->getMessage(),
+                );
+                return false;
+            }
+        }
+        return true;
     }
 }
--- libs/output-mapping/tests/Writer/Helper/PrimaryKeyHelperTest.php
+++ libs/output-mapping/tests/Writer/Helper/PrimaryKeyHelperTest.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace Keboola\OutputMapping\Tests\Writer\Helper;
 
+use Keboola\Csv\CsvFile;
 use Keboola\OutputMapping\Tests\AbstractTestCase;
+use Keboola\OutputMapping\Tests\Needs\NeedsEmptyOutputBucket;
 use Keboola\OutputMapping\Writer\Helper\PrimaryKeyHelper;
 use Psr\Log\NullLogger;
 
 class PrimaryKeyHelperTest extends AbstractTestCase
 {
+    private function createTable(array $columns, string $primaryKey): string
+    {
+        $csv = new CsvFile($this->temp->getTmpFolder() . '/import.csv');
+        $csv->writeRow($columns);
+        return $this->clientWrapper->getTableAndFileStorageClient()->createTableAsync(
+            $this->emptyOutputBucketId,
+            'test-table',
+            $csv,
+            ['primaryKey' => $primaryKey],
+        );
+    }
+
     /**
      * @dataProvider normalizePrimaryKeyProvider
      * @param array $pkey
@@ -36,5 +50,200 @@ class PrimaryKeyHelperTest extends AbstractTestCase
                 ['Id', 'Name'],
             ],
         ];
+    }
+
+    /**
+     * @dataProvider modifyPrimaryKeyDeciderOptionsProvider
+     */
+    public function testModifyPrimaryKeyDecider(
+        array $currentTableInfo,
+        array $newTableConfiguration,
+        bool $result,
+    ): void {
+        self::assertEquals($result, PrimaryKeyHelper::modifyPrimaryKeyDecider(
+            new NullLogger(),
+            $currentTableInfo['primaryKey'],
+            $newTableConfiguration['primary_key'],
+        ));
+    }
+
+    public function modifyPrimaryKeyDeciderOptionsProvider(): array
+    {
+        return [
+            [
+                [
+                    'primaryKey' => [],
+                ],
+                [
+                    'primary_key' => [],
+                ],
+                false,
+            ],
+            [
+                [
+                    'primaryKey' => [],
+                ],
+                [
+                    'primary_key' => ['Id'],
+                ],
+                true,
+            ],
+            [
+                [
+                    'primaryKey' => ['Id'],
+                ],
+                [
+                    'primary_key' => [],
+                ],
+                true,
+            ],
+            [
+                [
+                    'primaryKey' => ['Id'],
+                ],
+                [
+                    'primary_key' => ['Id'],
+                ],
+                false,
+            ],
+            [
+                [
+                    'primaryKey' => ['Id'],
+                ],
+                [
+                    'primary_key' => ['Name'],
+                ],
+                true,
+            ],
+            [
+                [
+                    'primaryKey' => ['Id'],
+                ],
+                [
+                    'primary_key' => ['Id', 'Name'],
+                ],
+                true,
+            ],
+        ];
+    }
+
+    #[NeedsEmptyOutputBucket]
+    public function testModifyPrimaryKeyChange(): void
+    {
+        $tableId = $this->createTable(['id', 'name', 'foo'], 'id,name');
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'name'], $tableInfo['primaryKey']);
+
+        PrimaryKeyHelper::modifyPrimaryKey(
+            $this->testLogger,
+            $this->clientWrapper->getTableAndFileStorageClient(),
+            $tableId,
+            ['id', 'name'],
+            ['id', 'foo'],
+        );
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf('Modifying primary key of table "%s" from "id, name" to "id, foo".', $tableId),
+        ));
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'foo'], $tableInfo['primaryKey']);
+    }
+
+    #[NeedsEmptyOutputBucket]
+    public function testModifyPrimaryKeyChangeFromEmpty(): void
+    {
+        $tableId = $this->createTable(['id', 'name', 'foo'], '');
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals([], $tableInfo['primaryKey']);
+
+        PrimaryKeyHelper::modifyPrimaryKey(
+            $this->testLogger,
+            $this->clientWrapper->getTableAndFileStorageClient(),
+            $tableId,
+            [ ],
+            ['id', 'foo'],
+        );
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf('Modifying primary key of table "%s" from "" to "id, foo".', $tableId),
+        ));
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'foo'], $tableInfo['primaryKey']);
+    }
+
+    #[NeedsEmptyOutputBucket]
+    public function testModifyPrimaryKeyChangeToEmpty(): void
+    {
+        $tableId = $this->createTable(['id', 'name', 'foo'], 'id,foo');
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'foo'], $tableInfo['primaryKey']);
+
+        PrimaryKeyHelper::modifyPrimaryKey(
+            $this->testLogger,
+            $this->clientWrapper->getTableAndFileStorageClient(),
+            $tableId,
+            $tableInfo['primaryKey'],
+            [],
+        );
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf('Modifying primary key of table "%s" from "id, foo" to "".', $tableId),
+        ));
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals([], $tableInfo['primaryKey']);
+    }
+
+    #[NeedsEmptyOutputBucket]
+    public function testModifyPrimaryKeyErrorRemove(): void
+    {
+        $tableId = $this->createTable(['id', 'name', 'foo'], 'id,name');
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'name'], $tableInfo['primaryKey']);
+        $invalidTableId = $tableId . '-non-existent';
+
+        PrimaryKeyHelper::modifyPrimaryKey(
+            $this->testLogger,
+            $this->clientWrapper->getTableAndFileStorageClient(),
+            $invalidTableId,
+            ['id', 'name'],
+            ['id', 'foo'],
+        );
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf('Modifying primary key of table "%s" from "id, name" to "id, foo".', $invalidTableId),
+        ));
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf(
+                'Error deleting primary key of table %s: The table "test-table-non-existent" ' .
+                'was not found in the bucket "%s" in the project',
+                $invalidTableId,
+                $this->emptyOutputBucketId,
+            ),
+        ));
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'name'], $tableInfo['primaryKey']);
+    }
+
+    #[NeedsEmptyOutputBucket]
+    public function testModifyPrimaryKeyErrorCreate(): void
+    {
+        $tableId = $this->createTable(['id', 'name', 'foo'], 'id,name');
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'name'], $tableInfo['primaryKey']);
+
+        PrimaryKeyHelper::modifyPrimaryKey(
+            $this->testLogger,
+            $this->clientWrapper->getTableAndFileStorageClient(),
+            $tableId,
+            ['id', 'name'],
+            ['id', 'bar'],
+        );
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf('Modifying primary key of table "%s" from "id, name" to "id, bar".', $tableId),
+        ));
+        self::assertTrue($this->testHandler->hasWarningThatContains(
+            sprintf(
+                'Error changing primary key of table %s: Primary key columns "bar" not found in "id, name, foo"',
+                $tableId,
+            ),
+        ));
+        $tableInfo = $this->clientWrapper->getTableAndFileStorageClient()->getTable($tableId);
+        self::assertEquals(['id', 'name'], $tableInfo['primaryKey']);
     }
 }

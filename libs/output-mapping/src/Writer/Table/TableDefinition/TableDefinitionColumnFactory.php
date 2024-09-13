8cffd0bdc18d048716fa1c192de8dd623f95d500
Erik Zigo <erik.zigo@keboola.com>
2024-09-13 11:15:05 +0200

    [output-mapping] TableDefinitionColumnFactory without NATIVE_BACKEND_TYPE_CLASS_MAP

--- libs/output-mapping/src/Writer/Table/TableDefinition/TableDefinitionColumnFactory.php
+++ libs/output-mapping/src/Writer/Table/TableDefinition/TableDefinitionColumnFactory.php
@@ -6,19 +6,10 @@ namespace Keboola\OutputMapping\Writer\Table\TableDefinition;
 
 use Keboola\Datatype\Definition\Common;
 use Keboola\Datatype\Definition\DefinitionInterface;
-use Keboola\Datatype\Definition\Exasol;
-use Keboola\Datatype\Definition\Snowflake;
-use Keboola\Datatype\Definition\Synapse;
 
 class TableDefinitionColumnFactory
 {
     public const NATIVE_TYPE_METADATA_KEY = 'KBC.datatype.backend';
-
-    public const NATIVE_BACKEND_TYPE_CLASS_MAP = [
-        'snowflake' => Snowflake::class,
-        'synapse' => Synapse::class,
-        'exasol' => Exasol::class,
-    ];
 
     /**
      * @var class-string<DefinitionInterface>|null
@@ -87,11 +78,14 @@ class TableDefinitionColumnFactory
      */
     private function getNativeDatatypeClass(array $tableMetadata, string $backend): ?string
     {
+        $columnDefinitionClassName = 'Keboola\\Datatype\\Definition\\' . ucfirst(strtolower($backend));
+
         $dataTypeBackend = $this->getDatatypeBackendFromMetadata($tableMetadata);
         if ($dataTypeBackend === $backend &&
-            array_key_exists($dataTypeBackend, self::NATIVE_BACKEND_TYPE_CLASS_MAP)
+            class_exists($columnDefinitionClassName) &&
+                is_subclass_of($columnDefinitionClassName, DefinitionInterface::class)
         ) {
-            return self::NATIVE_BACKEND_TYPE_CLASS_MAP[$backend];
+            return $columnDefinitionClassName;
         }
         return null;
     }

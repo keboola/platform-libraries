73ceba60b0098f9eeb683f1a68dbad92fd70d058
Erik Zigo <erik.zigo@keboola.com>
2025-01-04 13:56:40 +0100

    [output-mapping] removed usage of NeedsRemoveBucket satisfyer

--- libs/output-mapping/tests/Storage/BucketCreatorTest.php
+++ libs/output-mapping/tests/Storage/BucketCreatorTest.php
@@ -8,7 +8,7 @@ use Keboola\OutputMapping\Exception\InvalidOutputException;
 use Keboola\OutputMapping\Storage\BucketCreator;
 use Keboola\OutputMapping\SystemMetadata;
 use Keboola\OutputMapping\Tests\AbstractTestCase;
-use Keboola\OutputMapping\Tests\Needs\NeedsRemoveBucket;
+use Keboola\OutputMapping\Tests\Needs\NeedsEmptyInputBucket;
 use Keboola\OutputMapping\Tests\Writer\CreateBranchTrait;
 use Keboola\OutputMapping\Writer\Table\MappingDestination;
 use Keboola\StorageApiBranch\ClientWrapper;
@@ -18,12 +18,14 @@ class BucketCreatorTest extends AbstractTestCase
 {
     use CreateBranchTrait;
 
-    #[NeedsRemoveBucket('in.c-unexistsBucket')]
+    #[NeedsEmptyInputBucket]
     public function testEnsureDestinationBucket(): void
     {
+        $this->clientWrapper->getTableAndFileStorageClient()->dropBucket($this->emptyInputBucketId);
+
         $bucketCreator = new BucketCreator($this->clientWrapper);
 
-        $destination = new MappingDestination('in.c-unexistsBucket.testTable');
+        $destination = new MappingDestination($this->emptyInputBucketId . '.testTable');
         $systemMetadata = new SystemMetadata([
             'runId' => '123',
             'componentId' => 'test',
@@ -31,19 +33,21 @@ class BucketCreatorTest extends AbstractTestCase
         ]);
 
         $bucketInfo = $bucketCreator->ensureDestinationBucket($destination, $systemMetadata);
-        $this->assertEquals('in.c-unexistsBucket', $bucketInfo->id);
+        $this->assertEquals($this->emptyInputBucketId, $bucketInfo->id);
         $this->assertEquals('KBC.createdBy.component.id', $bucketInfo->metadata[0]['key']);
         $this->assertEquals('test', $bucketInfo->metadata[0]['value']);
         $this->assertEquals('KBC.createdBy.configuration.id', $bucketInfo->metadata[1]['key']);
         $this->assertEquals('456', $bucketInfo->metadata[1]['value']);
     }
 
-    #[NeedsRemoveBucket('in.c-main')]
+    #[NeedsEmptyInputBucket]
     public function testEnsureDestinationBucketExists(): void
     {
+        $this->clientWrapper->getTableAndFileStorageClient()->dropBucket($this->emptyInputBucketId);
+
         $bucketCreator = new BucketCreator($this->clientWrapper);
 
-        $destination = new MappingDestination('in.c-main.testTable');
+        $destination = new MappingDestination($this->emptyInputBucketId . '.testTable');
         $systemMetadata = new SystemMetadata([
             'runId' => '123',
             'componentId' => 'test',
@@ -54,15 +58,17 @@ class BucketCreatorTest extends AbstractTestCase
 
         // Second call - bucket already exists
         $bucketInfo = $bucketCreator->ensureDestinationBucket($destination, $systemMetadata);
-        $this->assertEquals('in.c-main', $bucketInfo->id);
+        $this->assertEquals($this->emptyInputBucketId, $bucketInfo->id);
     }
 
-    #[NeedsRemoveBucket('in.c-unexistsBucket')]
+    #[NeedsEmptyInputBucket]
     public function testEnsureDestinationBucketDevBranch(): void
     {
+        $this->clientWrapper->getTableAndFileStorageClient()->dropBucket($this->emptyInputBucketId);
+
         $bucketCreator = new BucketCreator($this->clientWrapper);
 
-        $destination = new MappingDestination('in.c-unexistsBucket.testTable');
+        $destination = new MappingDestination($this->emptyInputBucketId . '.testTable');
         $systemMetadata = new SystemMetadata([
             'runId' => '123',
             'componentId' => 'test',
@@ -87,8 +93,9 @@ class BucketCreatorTest extends AbstractTestCase
 
         $bucketCreator = new BucketCreator($this->clientWrapper);
 
-        $expectedMessage = 'Trying to create a table in the development bucket "in.c-unexistsBucket"';
-        $expectedMessage .= ' on branch "Keboola\OutputMapping\Tests\Storage\BucketCreatorTest"';
+        $expectedMessage = 'Trying to create a table in the development bucket "';
+        $expectedMessage .= $this->emptyInputBucketId;
+        $expectedMessage .= '" on branch "Keboola\OutputMapping\Tests\Storage\BucketCreatorTest"';
         $expectedMessage .= ' (ID "' . $branchId . '"), but the bucket is not assigned to any development branch.';
 
         $this->expectException(InvalidOutputException::class);

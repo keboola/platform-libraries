e5a958c63580ceb708604091cd4146a2c8823722
Ondrej Popelka <odinuv@gmail.com>
2025-04-16 23:05:35 +0200

    add permissions for WAII sessions https://keboola.atlassian.net/browse/KAB-958

---
+++ libs/permission-checker/src/Check/Ai/CanModifySessions.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\PermissionChecker\Check\Ai;
+
+use Keboola\PermissionChecker\Exception\PermissionDeniedException;
+use Keboola\PermissionChecker\Feature;
+use Keboola\PermissionChecker\PermissionCheckInterface;
+use Keboola\PermissionChecker\Role;
+use Keboola\PermissionChecker\StorageApiToken;
+
+class CanModifySessions implements PermissionCheckInterface
+{
+    public function checkPermissions(StorageApiToken $token): void
+    {
+        if ($token->hasFeature(Feature::PROTECTED_DEFAULT_BRANCH)) {
+            throw new PermissionDeniedException(sprintf(
+                'Role "%s" is not allowed to create AI sessions on protected branch projects',
+                $token->getRole()->value,
+            ));
+        } elseif ($token->isRole(Role::READ_ONLY)) {
+            throw PermissionDeniedException::roleDenied($token->getRole(), 'create AI sessions');
+        }
+    }
+}

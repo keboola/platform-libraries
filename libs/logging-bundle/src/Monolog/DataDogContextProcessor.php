37035409950d19709501726170a14b3373deb40c
Pepa Martinec <josef.martinec@keboola.com>
2025-04-24 17:24:48 +0200

    feat[logging-bundle]: Update to Monolog 3.0, drop Symfony 5.x support

--- libs/logging-bundle/src/Monolog/DataDogContextProcessor.php
+++ libs/logging-bundle/src/Monolog/DataDogContextProcessor.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace Keboola\LoggingBundle\Monolog;
 
+use Monolog\LogRecord;
 use Monolog\Processor\ProcessorInterface;
 
 use function DDTrace\current_context;
 
 class DataDogContextProcessor implements ProcessorInterface
 {
-    public function __invoke(array $record): array
+    public function __invoke(LogRecord $record): LogRecord
     {
         $context = current_context();
 
-        $record['dd'] = [
+        $record->extra['dd'] = [
             'trace_id' => $context['trace_id'],
             'span_id'  => $context['span_id'],
         ];

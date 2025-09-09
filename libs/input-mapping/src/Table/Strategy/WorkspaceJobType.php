3defadd45b9e357649327d7fff54c28dc7f4ace8
Erik Zigo <erik.zigo@keboola.com>
2025-09-09 15:20:20 +0200

    refactor (input-mapping): remove unused CLEAN case from WorkspaceJobType enum

--- libs/input-mapping/src/Table/Strategy/WorkspaceJobType.php
+++ libs/input-mapping/src/Table/Strategy/WorkspaceJobType.php
@@ -6,7 +6,6 @@ namespace Keboola\InputMapping\Table\Strategy;
 
 enum WorkspaceJobType: string
 {
-    case CLEAN = 'clean';     // Clean workspace before loading
     case CLONE = 'clone';     // Clone tables from Table Storage
     case LOAD = 'load';       // Load/copy tables from Table Storage
 }

95284ced45f59fd6c1c234c351a251782251059c
MiroCillik <mcillik@gmail.com>
2025-10-02 14:06:53 +0200

    tests (sanitizer): add more test cases to dataProvider for sanitize

--- libs/sanitizer/tests/ColumnNameSanitizerTest.php
+++ libs/sanitizer/tests/ColumnNameSanitizerTest.php
@@ -65,6 +65,71 @@ class ColumnNameSanitizerTest extends TestCase
                 'emoji😀name',
                 'emoji_name',
             ],
+            // Edge cases
+            [
+                '',
+                '',
+            ],
+            [
+                '   ',
+                '',
+            ],
+            [
+                '123',
+                '123',
+            ],
+            [
+                '_',
+                '',
+            ],
+            [
+                '__test__',
+                'test',
+            ],
+            // Valid characters boundary testing
+            [
+                'validName123',
+                'validName123',
+            ],
+            [
+                'Valid_Name_123',
+                'Valid_Name_123',
+            ],
+            [
+                'test@domain.com',
+                'test_domain_com',
+            ],
+            [
+                'user-id#123',
+                'user_id_123',
+            ],
+            // Consecutive invalid characters
+            [
+                'test!!!multiple!!!invalid',
+                'test_multiple_invalid',
+            ],
+            [
+                'name@@@with@@@symbols',
+                'name_with_symbols',
+            ],
+            // Leading underscore trimming
+            [
+                '___leading_underscores',
+                'leading_underscores',
+            ],
+            [
+                '123number_start',
+                '123number_start',
+            ],
+            // Mixed cases
+            [
+                'CamelCase123',
+                'CamelCase123',
+            ],
+            [
+                'snake_case_valid',
+                'snake_case_valid',
+            ],
         ];
     }
 }

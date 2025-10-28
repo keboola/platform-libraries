5eb0e9f5520c288b2e397dd796ecd5064bdb48f2
Ondřej Jodas <ondra.jodas@gmail.com>
2025-10-28 15:19:26 +0100

    Add validation for required environment variables in tests bootstrap

--- libs/query-service-api-client/tests/bootstrap.php
+++ libs/query-service-api-client/tests/bootstrap.php
@@ -9,3 +9,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotEnv = new Dotenv();
 $dotEnv->usePutenv();
 $dotEnv->bootEnv(dirname(__DIR__) . '/.env', 'dev', []);
+
+$requiredVars = [
+    'STORAGE_API_TOKEN',
+    'HOSTNAME_SUFFIX',
+    'STORAGE_API_URL',
+];
+
+foreach ($requiredVars as $var) {
+    if (empty($_ENV[$var])) {
+        throw new RuntimeException(
+            sprintf('Environment variable %s is required for functional tests', $var),
+        );
+    }
+}

24732005d54daca86381f3b15bc84178da4bb39b
Roman Pištěk <roman.pistek@keboola.com>
2026-01-22 17:26:50 +0100

    chore(messenger-bundle): Add delay to mitigate race condition

--- libs/messenger-bundle/provisioning/local/main.tf
+++ libs/messenger-bundle/provisioning/local/main.tf
@@ -19,6 +19,11 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 4.74.0"
     }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
   }
 
   backend "s3" {

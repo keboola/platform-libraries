43d3fb9a2b6e1577012379f7ce763e1c5cda9e6e
Pepa Martinec <josef.martinec@keboola.com>
2026-05-18 14:49:48 +0200

    feat(git-service-api-client)!: require httpsUrl on Repository model

--- libs/git-service-api-client/tests/Model/RepositoryTest.php
+++ libs/git-service-api-client/tests/Model/RepositoryTest.php
@@ -17,12 +17,38 @@ class RepositoryTest extends TestCase
             'createdAt' => '2026-04-28T10:00:00Z',
             'defaultBranch' => 'main',
             'sshUrl' => 'ssh://git@git-service/app-123.git',
+            'httpsUrl' => 'https://git-service/app-123.git',
         ]);
 
         self::assertSame('app-123', $repo->name);
         self::assertSame('2026-04-28T10:00:00Z', $repo->createdAt);
         self::assertSame('main', $repo->defaultBranch);
         self::assertSame('ssh://git@git-service/app-123.git', $repo->sshUrl);
+        self::assertSame('https://git-service/app-123.git', $repo->httpsUrl);
+    }
+
+    public function testFromResponseDataMissingHttpsUrl(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Repository::fromResponseData([
+            'name' => 'app-123',
+            'createdAt' => '2026-04-28T10:00:00Z',
+            'defaultBranch' => 'main',
+            'sshUrl' => 'ssh://git@git-service/app-123.git',
+            // httpsUrl missing
+        ]);
+    }
+
+    public function testFromResponseDataHttpsUrlWrongType(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Repository::fromResponseData([
+            'name' => 'app-123',
+            'createdAt' => '2026-04-28T10:00:00Z',
+            'defaultBranch' => 'main',
+            'sshUrl' => 'ssh://git@git-service/app-123.git',
+            'httpsUrl' => 123,
+        ]);
     }
 
     public function testFromResponseDataMissingField(): void

16673fde7ae9656f8123aee287f7fba9c92d5abe
Pepa Martinec <josef.martinec@keboola.com>
2026-07-09 13:52:06 +0200

    feat(api-bundle): add storage_client_options config node

--- libs/api-bundle/src/DependencyInjection/Configuration.php
+++ libs/api-bundle/src/DependencyInjection/Configuration.php
@@ -25,6 +25,42 @@ class Configuration implements ConfigurationInterface
                     ->values(array_map(fn(ServiceDnsType $v) => $v->value, ServiceDnsType::cases()))
                     ->defaultValue(ServiceDnsType::PUBLIC->value)
                 ->end()
+                ->arrayNode('storage_client_options')
+                    ->info(
+                        'Extra ClientOptions for the Storage API client factory: '
+                        . 'a service id (string) OR individual options.',
+                    )
+                    ->beforeNormalization()
+                        ->ifString()
+                        ->then(fn(string $v) => ['service' => $v])
+                    ->end()
+                    ->children()
+                        ->scalarNode('service')
+                            ->cannotBeEmpty()
+                            ->info('Service id of a ClientOptions instance merged onto the base options.')
+                        ->end()
+                        ->integerNode('backoff_max_tries')->end()
+                        ->integerNode('aws_retries')->end()
+                        ->booleanNode('aws_debug')->end()
+                        ->booleanNode('retry_on_maintenance')->end()
+                        ->booleanNode('use_branch_storage')->end()
+                        ->scalarNode('user_agent')
+                            ->cannotBeEmpty()
+                            ->info('Override the Storage client user agent (defaults to app_name).')
+                        ->end()
+                        ->scalarNode('logger')
+                            ->cannotBeEmpty()
+                            ->info('Service id of a PSR LoggerInterface to use instead of the default @logger.')
+                        ->end()
+                    ->end()
+                    ->validate()
+                        ->ifTrue(fn(array $v) => isset($v['service']) && count($v) > 1)
+                        ->thenInvalid(
+                            'storage_client_options: use either a service reference (string) '
+                            . 'or individual options, not both.',
+                        )
+                    ->end()
+                ->end()
             ->end()
         ;
 
---
+++ libs/api-bundle/tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\ApiBundle\Tests\DependencyInjection;
+
+use Keboola\ApiBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends TestCase
+{
+    /**
+     * @param array<mixed> $input
+     * @return array<mixed>
+     */
+    private function process(array $input): array
+    {
+        return (new Processor())->processConfiguration(new Configuration(), [$input]);
+    }
+
+    public function testStorageClientOptionsAbsentByDefault(): void
+    {
+        $config = $this->process([]);
+
+        self::assertArrayNotHasKey('storage_client_options', $config);
+    }
+
+    public function testStorageClientOptionsStringNormalizesToService(): void
+    {
+        $config = $this->process([
+            'storage_client_options' => 'app.storage_options',
+        ]);
+
+        self::assertSame(['service' => 'app.storage_options'], $config['storage_client_options']);
+    }
+
+    public function testStorageClientOptionsObjectFormParsesScalars(): void
+    {
+        $config = $this->process([
+            'storage_client_options' => [
+                'backoff_max_tries' => 10,
+                'aws_retries' => 5,
+                'aws_debug' => false,
+                'retry_on_maintenance' => true,
+                'use_branch_storage' => false,
+                'user_agent' => 'custom-ua/1.0',
+                'logger' => 'monolog.logger.storage_api',
+            ],
+        ]);
+
+        self::assertSame(
+            [
+                'backoff_max_tries' => 10,
+                'aws_retries' => 5,
+                'aws_debug' => false,
+                'retry_on_maintenance' => true,
+                'use_branch_storage' => false,
+                'user_agent' => 'custom-ua/1.0',
+                'logger' => 'monolog.logger.storage_api',
+            ],
+            $config['storage_client_options'],
+        );
+    }
+
+    public function testStorageClientOptionsServiceWithIndividualOptionIsRejected(): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage('use either a service reference');
+
+        $this->process([
+            'storage_client_options' => [
+                'service' => 'app.storage_options',
+                'backoff_max_tries' => 10,
+            ],
+        ]);
+    }
+
+    public function testStorageClientOptionsRejectsNonIntegerBackoff(): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+
+        $this->process([
+            'storage_client_options' => [
+                'backoff_max_tries' => 'not-a-number',
+            ],
+        ]);
+    }
+}
